f912f5503ed06169d37405d238178f8b584f9f87
Nickswagg <tngongangnicolas@gmail.com>
2020-06-05 09:31:06 -0400

    Completed game

---
+++ EtchASketch.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+
+public class EtchASketch extends Canvas
+{
+	int x, y;
+	Color cur;
+	
+	public static void main( String[] args )
+	{
+		JFrame win = new JFrame("Use the arrow keys!");
+		win.setSize(1024,768);
+		win.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		win.add( new EtchASketch() );
+		win.setVisible(true);
+	}
+
+	public EtchASketch()
+	{
+		enableEvents(java.awt.AWTEvent.KEY_EVENT_MASK);
+		requestFocus();
+		x = 100;
+		y = 300;
+		cur = Color.blue;
+	}
+
+	public void paint( Graphics g )
+	{
+		g.setColor(cur);
+		// Making the cercle smaller
+		g.fillOval(x, y, 25, 25);
+	}
+
+	public void update( Graphics g )
+	{
+		paint(g);
+	}
+
+	public void processKeyEvent(KeyEvent e)
+	{
+		// this method automatically gets called with they press a key
+		if ( e.getID() == KeyEvent.KEY_PRESSED )
+		{
+			// Making the circle move 10 pixels at a time and adding left and right directions
+			if ( e.getKeyCode() == KeyEvent.VK_UP )
+				y -= 10;
+			if ( e.getKeyCode() == KeyEvent.VK_DOWN )
+				y += 10;
+			if ( e.getKeyCode() == KeyEvent.VK_LEFT )
+				x -= 10;
+			if ( e.getKeyCode() == KeyEvent.VK_RIGHT )
+				x += 10;
+
+			// and we manually call paint() again to redraw
+			repaint();
+		}
+	}
+	
+	public boolean isFocusable()
+	{
+		return true;
+	}
+}
